3a0bf49e0053dd107c2ebeb05e858c514924137f
Andrew Savinykh <658865+AndrewSav@users.noreply.github.com>
2024-03-09 13:43:49 +1300

    add export function to Analyzer

--- Analyzer.cs
+++ Analyzer.cs
@@ -4,6 +4,10 @@ using lib.remnant2.saves.Model.Parts;
 using lib.remnant2.saves.Model.Properties;
 using lib.remnant2.saves.Navigation;
 using lib.remnant2.analyzer.Model;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
 
 namespace lib.remnant2.analyzer;
 
@@ -58,8 +62,84 @@ public partial class Analyzer
         return string.Join(", ", GetProfileStrings(folderPath));
     }
 
-    public static void Export(string targetFolder, string? folderPath = null)
+    public class IgnorePropertiesResolver(IEnumerable<string> propNamesToIgnore) : DefaultContractResolver
     {
+        private readonly HashSet<string> ignoreProps = new(propNamesToIgnore);
+
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            JsonProperty property = base.CreateProperty(member, memberSerialization);
+            if (ignoreProps.Contains(property.PropertyName!))
+            {
+                property.ShouldSerialize = _ => false;
+                return property;
+            }
+
+            if (property.DeclaringType == typeof(Property))
+            {
+                property.ShouldSerialize = x =>
+                {
+                    Property p = (Property)x;
+                    return p.Name.Name != "FowVisitedCoordinates";
+                };
+                return property;
+
+            }
+            return property;
+        }
+    }
+    private static SaveFile ExportFile(string targetFolder, string sourcePath, bool exportCopy, bool exportDecoded, bool exportJson)
+    {
+        if (exportCopy)
+        {
+            File.Copy(sourcePath, Path.Combine(targetFolder, Path.GetFileName(sourcePath)),true);
+        }
+        
+        string fileName = Path.GetFileNameWithoutExtension(sourcePath);
+        SaveFile sf = SaveFile.Read(sourcePath);
+        
+        if (exportDecoded)
+        {
+            SaveFile.WriteUncompressed(Path.Combine(targetFolder, $"{fileName}.dec"), sf);
+        }
+
+        if (exportJson)
+        {
+            JsonSerializer serializer = new()
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new IgnorePropertiesResolver([
+                    "ReadOffset",
+                    "WriteOffset",
+                    "ReadLength",
+                    "WriteLength",
+                    "ExtraComponentsData",
+                    "ExtraPropertiesData"
+                ]),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            using StreamWriter sw = new(Path.Combine(targetFolder, $"{fileName}.json"));
+            using JsonWriter writer = new JsonTextWriter(sw);
+            serializer.Serialize(writer, sf.SaveData);
+        }
+        return sf;
+    }
+
+    public static void Export(string targetFolder, string? folderPath, bool exportCopy, bool exportDecoded, bool exportJson)
+    {
+        string folder = folderPath ?? Utils.GetSteamSavePath();
+        SaveFile profileSf = ExportFile(targetFolder, Path.Combine(folder, "profile.sav"), exportCopy, exportDecoded, exportJson);
+
+        Navigator profile = new(profileSf);
+        ArrayProperty ap = profile.GetProperty("Characters")!.Get<ArrayProperty>();
+
+        for (int index = 0; index < ap.Items.Count; index++)
+        {
+            object? item = ap.Items[index];
+            ObjectProperty ch = (ObjectProperty)item!;
+            ExportFile(targetFolder, Path.Combine(folder, $"save_{profile.Lookup(ch).Path[^1].Index}.sav"), exportCopy, exportDecoded, exportJson);
+        }
     }
 
     public static Dataset Analyze(string? folderPath = null)
