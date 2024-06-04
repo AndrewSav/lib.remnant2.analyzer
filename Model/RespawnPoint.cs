439e84b80dedc81a6a01ca1abc3d007cbab3ebce
Angelore <angelore.main@gmail.com>
2024-06-04 16:24:28 +0300

    Add RespawnPoint class

---
+++ Model/RespawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using lib.remnant2.analyzer.Enums;
+
+namespace lib.remnant2.analyzer.Model;
+public class RespawnPoint
+{
+    public RespawnPoint(string? name, RespawnPointType type)
+    {
+        Name = name;
+        Type = type;
+    }
+
+    public string? Name { get; set; }
+    public RespawnPointType Type { get; set; }
+}
